ed5757cf32e79969744d4269afae0d1960b00032
ChrislayneGC <dev.chrislayne@gmail.com>
2023-09-04 21:51:11 -0300

    adição do css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 //Componente React (Funcão JavaScript)
 import React from "react";
+import "./App.css"; // Importe seu arquivo CSS
 
-function Home() {
+function App() {
   return (
-    <div className="home-container">
+    <div className="container">
       <h1 className="main-heading">Mantenha a Persistência</h1>
       <p className="sub-heading">
         A jornada pode ser difícil, mas a perseverança é a chave para o sucesso.
@@ -12,6 +13,7 @@ function Home() {
   );
 }
 
+export default App;
+
 //Dentro de um arquivo JS/TS eu posso ter varias funções declaradas, sinal para que a função HOME é a que a gente quer usar para renderizar a pág.
 //Obs: default: Padrão
-export default Home;
